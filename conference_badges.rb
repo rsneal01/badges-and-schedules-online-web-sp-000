9cdf21c2fe348f755259b699f1abf32e9f1f1e90
Bobby Neal <rsneal01@gmail.com>
2020-04-26 17:11:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,9 +25,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-    batch_badge_creator(attendees)
-    badges.each do |name|
-      puts badges
-  end
+    batch_badge_creator(attendees).each do |badge|
+      puts badge
+    end
 end
   
